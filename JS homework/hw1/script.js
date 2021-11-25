ad5ecad9c8a41fd4f3a46667f1d5b2e4d253c269
timurza <timur88info@gmail.com>
2021-11-25 16:29:19 +0300

    task3 added

--- JS homework/hw1/script.js
+++ JS homework/hw1/script.js
@@ -1,6 +1,6 @@
 //1 задача
 
-/*let x = 1,
+let x = 1,
       y = 2;
 console.log(x,y);
 [x, y] = [y, x];
@@ -10,21 +10,16 @@ console.log( x,y);
 
 let arr = [5,10,2,12,3,16,21,25,28,32];
 let result = (arr[0]+arr[4]+arr[9])
- console.log(result);*/
+ console.log(result);
 
 //3 задача
 
 function func() {
-let x = 1,
-y = 2;
-[x, y] = [y, x];
-
-
-let arr = [5,10,2,12,3,16,21,25,28,32];
-let result = (arr[0]+arr[4]+arr[9]);
-
-
-let funcObj = (x,y,result);
+    let x = 1,
+        y = 2;
+    [x, y] = [y, x];
+    let arr = [5, 10, 2, 12, 3, 16, 21, 25, 28, 32];
+    let result = arr[0] + arr[4] + arr[9];
+    return {x, y, result};
 }
-
-console.log(funcObj);
+console.log(func());
