1fef0b8e83f3cceaeb6b3b4b75b15c33bcc78b94
timurza <timur88info@gmail.com>
2021-11-25 14:49:53 +0300

    js folder added

---
+++ JS homework/hw1/script.js
@@ -0,0 +1,30 @@
+//1 задача
+
+/*let x = 1,
+      y = 2;
+console.log(x,y);
+[x, y] = [y, x];
+console.log( x,y); 
+
+//2 задача
+
+let arr = [5,10,2,12,3,16,21,25,28,32];
+let result = (arr[0]+arr[4]+arr[9])
+ console.log(result);*/
+
+//3 задача
+
+function func() {
+let x = 1,
+y = 2;
+[x, y] = [y, x];
+
+
+let arr = [5,10,2,12,3,16,21,25,28,32];
+let result = (arr[0]+arr[4]+arr[9]);
+
+
+let funcObj = (x,y,result);
+}
+
+console.log(funcObj);
